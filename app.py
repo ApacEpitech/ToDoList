2418b4464e77462923bf8efb1f56c14d7d9a7f0a
Enzo Scaduto <enzo.scaduto@gmail.com>
2019-11-06 15:23:07 +0100

    Fix CORS for local request

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask_pymongo import PyMongo
 from pymongo import errors
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.secret_key = "passwordGoesHere"
 app.config["MONGO_URI"] = "mongodb://localhost:27017/todolist"
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+
 
 try:
     mongo = PyMongo(app)
